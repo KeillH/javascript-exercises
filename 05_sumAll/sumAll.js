af8c5dfd8d5d347adc1fa9c3c5595d4327a4c4a6
Keill Hopkins <keill.j.hopkins@gmail.com>
2022-07-29 09:14:00 +1000

    Modeule 05 complete: lot of if statements used to handle the errors. This might be able to be shortened in the future

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(n1,n2) {
+    let start;
+    let end;
+    //Checking for errors 
+    if (n1<0 || n2<0){
+        return 'ERROR';
+    }
+    if (typeof(n1)!='number' || typeof(n2)!='number'){
+        return 'ERROR';
+    }
+
+    //Asigning values in order
+    if (n1<n2){
+        start = n1;
+        end = n2;
+    }else {
+        start = n2;
+        end = n1;
+    }
+    total=0;
+    for (let i=start;i<=end;i++){
+        total +=i;
+    }
+    return total;
 
 };
 
