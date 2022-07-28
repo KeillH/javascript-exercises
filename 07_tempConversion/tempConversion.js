2768bb19d115aab7eab3480e0cccede7b3891b86
Keill Hopkins <keill.j.hopkins@gmail.com>
2022-07-29 09:41:52 +1000

    completed module 07: parseFloat was used to remove trailing 0's left behing by toFixed()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,10 @@
-const ftoc = function() {
+const ftoc = function(temperature) {
+  return parseFloat(((temperature - 32)*5/9).toFixed(1));
 
 };
 
-const ctof = function() {
+const ctof = function(temperature) {
+  return parseFloat((temperature*9/5+32).toFixed(1));
 
 };
 
