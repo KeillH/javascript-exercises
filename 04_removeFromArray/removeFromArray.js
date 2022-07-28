378cb8b6096b527d32f5e3d51ac1e0f41febfd26
Keill Hopkins <keill.j.hopkins@gmail.com>
2022-07-29 08:55:37 +1000

    Module 4 completed. Code takes the aruments and checks the array to see if any arguments remain in it. And if so they are removed. It can handle multiple occurances of the same argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    //For each argument to be removed from the array. The code checks to see if it is present and removes the value if it is.
+    //If an argument is there it will continuously check the same argument until all multiples of it are removed and then will head to the next argument
+    for (let i =0; i<args.length;i++){
+        let searcher = 0; //Variable which is used to let the loop know if an item has been found or not
+        while (searcher != -1){
+            searcher = array.indexOf(args[i]);
+            if (searcher === -1){
+                continue;
+            } else {
+                array.splice(searcher,1);
+            }
+        }
+    }
+    return array;
 
 };
 
