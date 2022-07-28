1323dcd4bc23cf2a77ee761630d94ae93e9827f3
Keill Hopkins <keill.j.hopkins@gmail.com>
2022-07-28 11:22:53 +1000

    Module 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+    let output;
+    if (string.length === 0){
+        output = '';
 
+    }
+    if (n>=0){
+        output = string.repeat(n);
+    }
+    if (n<0){
+        output = 'ERROR';
+    }
+    return output;
 };
 
 // Do not edit below this line
