5e516187232e52f0117ddf333d7eff135a21d235
Keill Hopkins <keill.j.hopkins@gmail.com>
2022-07-29 09:54:09 +1000

    module 08 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,40 @@
-const add = function() {
+const add = function(n1,n2) {
+  return n1+n2;
 	
 };
 
-const subtract = function() {
+const subtract = function(n1,n2) {
+  return n1-n2;
 	
 };
 
-const sum = function() {
+const sum = function(arraySum) {
+  let total=0;
+  for (let i of arraySum){
+    total+=i;
+  }
+  return total;
+};
+
+const multiply = function(arrayMultiply) {
+  let total = arrayMultiply[0];
+  for (let i=1;i<arrayMultiply.length;i++){
+    total*=arrayMultiply[i];
+  }
+  return total;
+};
+
+const power = function(n1,n2) {
+  return n1**n2;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(number) {
+  total = 1;
+  for (let i =1;i<=number;i++){
+    total*=i;
+  }
+  return total;
 	
 };
 
