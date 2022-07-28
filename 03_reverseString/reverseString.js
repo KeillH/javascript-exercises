4a36d92b2c016b49c88ceb4ae1cd3a95476e9e4e
Keill Hopkins <keill.j.hopkins@gmail.com>
2022-07-28 18:28:17 +1000

    03 module completed. Used a for loop through the given string starting from the end moving to the front. Each char was pushed to an aray and then the array was joined to return the reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedArray = []
+
+    for (let i =string.length-1;i>=0;i--){
+        reversedArray.push(string[i]);
+    }
+    let reverse = reversedArray.join("");
+
+
+    return reverse;
 
 };
 
