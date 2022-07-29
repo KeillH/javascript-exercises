76de5f3a59cbaf0f9b01e54656d1a107dcb72548
Keill Hopkins <keill.j.hopkins@gmail.com>
2022-07-29 10:39:54 +1000

    Module 09 completed: had to hard code the checks to remove punctuation, however, i tried to use regex and .replace but i couldn't get it to work. This would produce a more robust solution if that was to be used

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,35 @@
-const palindromes = function () {
+const palindromes = function (forwardString) {
+    //Reversing the string
+    let reverseStingArray =[];
+    let reverseString;
+    let forwardStringArray = [];
+
+    //conditioning the forward string to have the punctuation and capitalisation removed and handled correctly
+    forwardString = forwardString.toLowerCase();
+    for (let i = 0;i<forwardString.length;i++){
+        if (forwardString[i] ==='!'||forwardString[i] ==='.'||forwardString[i] ===','||forwardString[i] ===' '){
+            continue;
+        }else {
+            forwardStringArray.push(forwardString[i]);
+        }
+    }
+    forwardString = forwardStringArray.join('');
+    
+
+    for (i =forwardString.length-1;i>=0;i--){
+        reverseStingArray.push(forwardString[i]);
+        
+    }
+    reverseString = reverseStingArray.join('');
+    console.log(reverseString);
+
+    if (forwardString === reverseString){
+        return true;
+    }else {
+        return false;
+    }
+    
+
 
 };
 
