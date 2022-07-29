0ce9c71d89141857974dcb8def8af4ad9c4a03ea
Keill Hopkins <keill.j.hopkins@gmail.com>
2022-07-29 10:59:05 +1000

    Module 10 complete: pretty straight forwad. Not too much difficult

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = parseInt(number,10);
+    if (number === 0){return 0;}
+    if (number <0){return 'OOPS'}
+
+    let fibPrevious = 0;
+    let fibCurrent = 1;
+    let temp;
+    for (let i =1;i<number;i++){
+        temp = fibCurrent;
+        fibCurrent = fibCurrent + fibPrevious;
+        fibPrevious = temp;
+    }
+
+
+    return fibCurrent;
 
 };
 
