48bbbb7c9f051f413cb1521d9bbbaf65739ab110
Keill Hopkins <keill.j.hopkins@gmail.com>
2022-08-08 10:18:33 +1000

    solution to 12: remember to define variables outside of loops and if statements if they need to be accesed outside of the loop

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    //Accepts array of objects
+    const orderedPeople = people.sort((a,b) => {
+        let ageA;
+        let ageB;
+        currentYear = new Date().getFullYear();
+        if ('yearOfDeath' in a){
+            ageA = a.yearOfDeath -a.yearOfBirth;
+        }else {
+            ageA = currentYear-a.yearOfBirth;
+        }
+
+        if ('yearOfDeath' in b){
+            ageB = b.yearOfDeath -b.yearOfBirth;
+        }else {
+            ageB = currentYear-b.yearOfBirth;
+        }
+        return ageA > ageB ? -1: 1;
+
+    });
+
+
+    return people[0];
 
 };
 
